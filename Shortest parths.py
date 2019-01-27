ec3dfc7e61e3290010df88147becbf4abdea5a4c
MichaelZhangty <36727768+MichaelZhangty@users.noreply.github.com>
2019-01-27 18:30:23 -0500

    floormap is the map for floor -1 and 0 and 1

--- Shortest parths.py
+++ Shortest parths.py
@@ -1,8 +1,11 @@
 from intersection import Intersection
 from block import Block
-from floormap1 import blocklst, intersectionlst
+from floormap import blocklst, intersectionlst
 import queue as Queue
 from collections import defaultdict
+import bintrees
+
+
 
 
 class PriorityQueue(object):
@@ -19,10 +22,18 @@ class PriorityQueue(object):
 
         # for inserting an element in the queue
 
-    def insert(self, vertex,weight):
-        self.queue.append([vertex,weight])
+    def insert(self, vertex, weight):
+        self.queue.append([vertex, weight])
 
         # for popping an element based on Priority
+
+    def update(self,vertex,prevweight,newweight):
+        for var in range(len(self.queue)):
+            if self.queue[var] == [vertex,prevweight]:
+                del self.queue[var]
+                self.queue.append([vertex,newweight])
+
+
 
     def delete(self):
         try:
@@ -39,11 +50,11 @@ class PriorityQueue(object):
 
 
 
-def dijsktra(A,start):
-    dist =[]
-    prev =[]
+def dijsktra(A, start, intersectionlist):
+    dist = []
+    prev = []
     for i in range(len(A)):
-        dist.append(1000000)
+        dist.append(10000)
         prev.append("null")
     dist[start] = 0
     Q = PriorityQueue()
@@ -52,53 +63,62 @@ def dijsktra(A,start):
     while not Q.isEmpty():
         w = Q.delete()
         for edge in A[w[0]]:
-            if type(edge) != int:
-                tmp = dist[w[0]] + edge[1]
+            #print(edge)
+            if type(edge) == list:
+                #print(edge)
+                tmp = dist[w[0]] + edge[1] + intersectionlist[edge[0]].length * 1/2
                 if tmp < dist[edge[0]]:
+                    Q.update(edge[0],dist[edge[0]],tmp)
                     dist[edge[0]] = tmp
                     prev[edge[0]] = w[0]
-    return dist , prev
+    return dist, prev
 
 
-Adjlst = []
-n = (len(intersectionlst))
-for k in range(n):
-    Adjlst.append([k])
-edgelst = []
-intersectionnode = []
-for inter in intersectionlst:
-    intersectionnode.append([inter,inter.all_blocks])
-for v in range(len(intersectionnode)):
-    for block1 in intersectionnode[v][1]:
-        for j in range(len(intersectionnode)):
-            for block2 in intersectionnode[j][1]:
-                if block1.A[0] == block2.A[0] and j != v and block1.A[1] == block2.A[1]:
-                    if block1.A[1]-block1.B[1] != 0:
-                        edgelst.append([j,v,abs(block1.A[1]-block1.B[1])])
-                        edgelst.append([v,j,abs(block2.A[1]-block2.B[1])])
-                    else:
-                        edgelst.append([j, v, abs(block1.A[0] - block1.B[0])])
-                        edgelst.append([v, j, abs(block2.A[0] - block2.B[0])])
+def makeadjlst(intersectionlist):
+    Adjlst = []
+    n = len(intersectionlist)
+    for k in range(n):
+        Adjlst.append([k])
+    edgelst = []
+    intersectionnode = []
+    for inter in intersectionlist:
+        intersectionnode.append([inter, inter.all_blocks])
+    for v in range(len(intersectionnode)):
+        for block1 in intersectionnode[v][1]:
+            for j in range(len(intersectionnode)):
+                for block2 in intersectionnode[j][1]:
+                    if block1.A[0] == block2.A[0] and j != v and block1.A[1] == block2.A[1] and block1.B[0] == block2.B[0] and block1.B[1] == block2.B[1] and block1.A[2] == block2.A[2] and block1.B[2] == block2.B[2]:
+                        if block1.floor == 1 or block1.floor == 0 or block1.floor == -1:
+                            if block1.A[1] - block1.B[1] != 0:
+                                edgelst.append([j, v, abs(block1.A[1] - block1.B[1])])
+                            else:
+                                edgelst.append([j, v, abs(block1.A[0] - block1.B[0])])
+                        else:
+                            if block1.A[1] - block1.B[1] != 0:
+                                edgelst.append([j, v, abs(block1.A[1] - block1.B[1]) * 2])
+                            else:
+                                edgelst.append([j, v, abs(block1.A[0] - block1.B[0]) * 2])
 
-for edge in edgelst:
-    Adjlst[edge[0]].append([edge[1],edge[2]])
+    for edge in edgelst:
+        Adjlst[edge[0]].append([edge[1], edge[2]])
+    return Adjlst
 
 
-# exit in floor 1 is intercation beghimno
-# the representative number is 2,5,7,8,9,13,14,15
-# we need to do minus 1 because the list begins with 0
-exitlst = [1,4,6,7,8,12,13,14]
-dist2,prev2= dijsktra(Adjlst,1)
-dist5,prev5= dijsktra(Adjlst,4)
-dist7,prev7= dijsktra(Adjlst,6)
-dist8,prev8= dijsktra(Adjlst,7)
-dist9,prev9= dijsktra(Adjlst,8)
-dist14,prev14= dijsktra(Adjlst,12)
-dist15,prev15= dijsktra(Adjlst,13)
-dist16,prev16= dijsktra(Adjlst,14)
-distlst = [dist2,dist5,dist7,dist9,dist14,dist15,dist16]
-prevlst = [prev2,prev5,prev7,prev9,prev14,prev15,prev16]
-
+# exit is intersection 0,9 14,30,34
+Adjlst = makeadjlst(intersectionlst)
+exitlst = [0,9,14,30,34]
+distlst = []
+prevlst = []
+for num in exitlst:
+    dist, prev = dijsktra(Adjlst, num, intersectionlst)
+    distlst.append(dist)
+    prevlst.append(prev)
+# print(prevlst[0])
+# dist0,prev0= dijsktra(Adjlst,0)
+# dist9,prev9= dijsktra(Adjlst,9)
+# istlst = [dist0,dist9]
+# prevlst = [prev0,prev9]
+n = len(intersectionlst)
 finallst = []
 for s in range(n):
     finallst.append([])
@@ -110,14 +130,18 @@ for k in range(n):
         if mini > distlst[g][k]:
             mini = distlst[g][k]
             minipath = prevlst[g]
-            finallst[k] = [mini,minipath]
-total = [] # total is the path to the exit for each intersection
+            finallst[k] = [mini, minipath]
+total = []  # total is the path to the exit for each intersection
 for num in range(n):
     lst = []
     path = finallst[num][1]
     start = num
     while start not in exitlst:
-        start = path[start]
-        lst.append(start)
+        if start == "null":
+            break
+        else:
+            lst.append(start)
+            start = path[start]
+    lst.append(start)
     total.append(lst)
 print(total)
